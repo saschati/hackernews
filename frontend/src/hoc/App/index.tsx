348cbe965e7ac66fb2ea4b554fa64a633c9359f5
olexander <Olexander>
2023-01-23 19:10:39 +0200

    Change scss function and add new config

--- frontend/src/hoc/App/index.tsx
+++ frontend/src/hoc/App/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { VechaiProvider, Button } from '@vechaiui/react'
 
-const App: React.FC = () => {
+const App: React.FC = (): JSX.Element => {
   return (
     <VechaiProvider>
       <Button>test</Button>
